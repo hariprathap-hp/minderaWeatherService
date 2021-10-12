23a99cffd6edeaae1248b16f414e5bbc0c724b4f
hariprathap-hp <hariprathap89@gmail.com>
2021-10-12 11:39:34 +0530

    modify weather stack key

--- config/keyConfig/config.go
+++ config/keyConfig/config.go
@@ -2,5 +2,5 @@ package keyConfig
 
 const (
 	OPENWEATHER_API_KEY   = "congifure openweather key"
-	WEATHER_STACK_API_KEY = "57d27e6e2e86b46e0582e77ccf92661c"
+	WEATHER_STACK_API_KEY = "configure weather stack key"
 )
